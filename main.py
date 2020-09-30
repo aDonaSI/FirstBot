10ba14454b712c2c654ef4834a39ddd68a1e2ead
aDonaSI <adona@ensc.fr>
2020-09-30 15:15:31 +0200

    test 6 function move

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from wheel import move_straight, lock_wheel,move
 import time
 delay=1
 for k in range(10):
-    move(1,(3.14/10),delay)
+    move(10,(3.14/10),delay)
     time.sleep(delay)
 lock_wheel()
