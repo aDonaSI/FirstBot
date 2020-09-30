1c7361abd0f51e7bb13ff4b904781441e6f7a75e
aDonaSI <adona@ensc.fr>
2020-09-30 17:18:20 +0200

    test 6 function go_to

--- go_to.py
+++ go_to.py
@@ -10,16 +10,21 @@ lin_speed=20
 
 def go_to(x,y,angle):
     global x0,y0,angle0,delay,speed,ang_speed,lin_speed
-    move(x-x0,0,(x-x0)/lin_speed)
+    move(x-x0,0,abs(x-x0)/lin_speed)
     time.sleep(abs(x-x0)/lin_speed)
-    move(0,3.14/2,(3.14/2)/ang_speed)
+
+    move(0,3.14/2,abs(3.14/2)/ang_speed)
     time.sleep(abs(3.14/2)/ang_speed)
-    move(y-y0,0,(y-y0)/lin_speed)
+
+    move(y-y0,0,abs(y-y0)/lin_speed)
     time.sleep(abs(y-y0)/lin_speed)
-    move(0,-3.14/2,(-3.14/2)/ang_speed)
+
+    move(0,-3.14/2,abs(-3.14/2)/ang_speed)
     time.sleep(abs(-3.14/2)/ang_speed)
-    move(0,angle-angle0,(angle-angle0)/ang_speed)
+
+    move(0,angle-angle0,abs(angle-angle0)/ang_speed)
     time.sleep(abs(angle-angle0)/ang_speed)
+
     lock_wheel()
 
     x0=x0-x
