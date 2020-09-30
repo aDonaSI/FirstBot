e53a85580e366565a0ea44bb41ed26b1f1cc6d99
aDonaSI <adona@ensc.fr>
2020-09-30 14:42:43 +0200

    modif wheel.py for test 1

--- wheel.py
+++ wheel.py
@@ -1,14 +1,11 @@
 import pypot.dynamixel
 speed=0
+ports = pypot.dynamixel.get_available_ports()
+if not ports:
+    exit('No port')
 
-
-def connect():
-    ports = pypot.dynamixel.get_available_ports()
-    if not ports:
-        exit('No port')
-
-    dxl_io = pypot.dynamixel.DxlIO(ports[0])
-    dxl_io.set_wheel_mode([1,2])
+dxl_io = pypot.dynamixel.DxlIO(ports[0])
+dxl_io.set_wheel_mode([1,2])
 
 def free_wheel():
     dxl_io.set_joint_mode([1,2])
