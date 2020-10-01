ac18e9c0e39b63871819d339e97a3e8b87eadf39
aDonaSI <adona@ensc.fr>
2020-10-01 12:59:11 +0200

    test bis bis bis bis bis

--- odom.py
+++ odom.py
@@ -13,7 +13,7 @@ def direct_kinematics(vg,vd) :
     vg *= r * 2 * math.pi / 44.81
     vmin, vmax = min(vg, vd), max(vg, vd)
     xpoint=vmin + (vmax-vmin)/2
-    thetapoint=(vd-vg)/17.9
+    thetapoint=(vd-vg)/18
     return xpoint,thetapoint
 
 def odom(dt, xpoint,thetapoint) :
