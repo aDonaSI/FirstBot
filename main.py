a75c6af2c79a82070cba8bf89743af5643a29494
aDonaSI <adona@ensc.fr>
2020-10-02 10:16:17 +0200

    test integaration camera 14

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def follow_line():
         #odom_qqch(get_current_color())
         t1 = time.time()
         print("time cost:", t1-t0, "distance image:", distance)
-        follow(320-distance, .5)
+        follow(distance, .5)
 
 follow_line()
 end_camera()
