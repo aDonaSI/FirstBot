a7a6fdf34159d413e339533d47814575cfc37114
aDonaSI <adona@ensc.fr>
2020-10-02 08:34:42 +0200

    test integaration camera 1

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from wheel import move_straight, lock_wheel,move,free_wheel
 from go_to import go_to,go_to_fancy,follow
+import suivi
 import time
 import math
 
 from odom import odom_update, odom_get, idk
 
-delay=1
+delay=0.1
 # for k in range(10):
 #     move(20,3.14/10,delay)
 #     time.sleep(delay)
@@ -16,18 +17,45 @@ delay=1
 # go_to(-8,5,-(3.14/4))
 # lock_wheel()
 
-try:
-    x, y, angle = go_to_fancy(10, 10, 0)
-    print(x, y, angle)
-    free_wheel()
-except Exception as e:
-    lock_wheel()
-    print(e)
+# try:
+#     x, y, angle = go_to_fancy(10, 10, 0)
+#     print(x, y, angle)
+#     free_wheel()
+# except Exception as e:
+#     lock_wheel()
+#     print(e)
 
-# while True:
-#     odom_update(.1)
-#     print(odom_get())
-#     time.sleep(.1)
+def odom():
+    while True:
+    odom_update(.1)
+    print(odom_get())
+    time.sleep(.1)
+
+def follow_line():
+    while True:
+        ret, frame = cap.read()
+        src = frame[160:]
+        green_processing(frame)
+        dataset = color_pixel_coord(frame)
+
+        if (len(dataset)>0):
+            centers = kmeans(dataset,2)
+            lastcenter = behavior(centers,lastcenter)
+            follow(lastcenter,delay)
+    #Testing
+            cv2.circle(src,(int(centers[0][1]),int(centers[0][0])),1,(255,0,255),3)
+            cv2.circle(src,(int(centers[1][1]),int(centers[1][0])),1,(255,0,255),3)
+        print("COLOR = ",COLOR,"\n")
+        cv2.imshow("Frame", src)
+    #EO Testing
+
+        key = cv2.waitKey(1)
+        if key==50:
+            free_wheel()
+            break
+
+
+follow_line()
 
 #idk()
 
