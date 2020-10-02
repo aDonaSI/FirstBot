42c3a218b984561a9db53751a0e87a34637bc1ed
Deleglise Alexandre <Alexandre.Deleglise@bordeaux-inp.fr>
2020-10-02 10:21:12 +0200

    changement dans main

--- main.py
+++ main.py
@@ -1,70 +1,57 @@
 from wheel import move_straight, lock_wheel,move,free_wheel
 from go_to import go_to,go_to_fancy,follow
 from suivi import get_distance_suivi, get_current_color, end_camera
+from odom import odom_update, odom_get, print_circuit
+
 import time
 import math
 
-from odom import odom_update, odom_get, idk
 
-delay=0.1
-# for k in range(10):
-#     move(20,3.14/10,delay)
-#     time.sleep(delay)
-# lock_wheel()
+def time_exec(f, args):
+    before = time.time()
+    f(args)
+    return time.time() - before
 
-# go_to(8,-5,(3.14/4))
-# lock_wheel()
-# go_to(-8,5,-(3.14/4))
-# lock_wheel()
-
-# try:
-#     x, y, angle = go_to_fancy(10, 10, 0)
-#     print(x, y, angle)
-#     free_wheel()
-# except Exception as e:
-#     lock_wheel()
-#     print(e)
-
-def odom():
-    while True:
-        odom_update(.1)
-        print(odom_get())
-        time.sleep(.1)
-
-
-def follow_line():
-    while 1:
-        t0 = time.time()
+## tasks
+def task_follow():
+    distance_in_image
+    def loop_analyse():
         distance = get_distance_suivi()
-        #odom_qqch(get_current_color())
-        t1 = time.time()
-        follow(distance, t1-t0)
+    def loop_odom(dt):
+        odom_follow(dt, get_current_color())
 
-follow_line()
+    while get_current_color() < 4:
+        time_cost = time_exec(loop_analyse)
+        time_cost+= time_exec(loop_odom, time_cost)
+        follow(distance_in_image, time_cost)
+
+    print_circuit()
+
+def task_goto(x, y, theta):
+    go_to_fancy(x, y, theta)
+
+def task_odom():
+    dt = .1
+    def loop():
+        odom_update(dt)
+        print(odom_get())
+
+    while True:
+        time_cost = time_exec(loop)
+        time.sleep(dt - time_cost)
+
+
+task_follow() # tmp
+
+## __main__
+# if sys.argv[1] == "follow":
+#     task_follow()
+#
+# elif sys.argv[1] == "goto":
+#     x, y, theta = float(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4])
+#     task_goto()
+#
+# elif sys.argv[1] == "odom":
+#     task_odom()
+
 end_camera()
-
-#idk()
-
-# time.sleep(2)
-# go_to_fancy(10,-10,(3.14))
-# lock_wheel()
-# time.sleep(2)
-# go_to_fancy(0,0,0)
-# free_wheel()
-
-# move(20,0,5)
-
-# #move_straight(44.81)
-# time.sleep(5)
-# lock_wheel()
-
-# for k in range(10):
-#     follow(160,delay)
-#     time.sleep(delay)
-
-# for k in range(10):
-#     follow(160-(k*16),delay)
-#     time.sleep(delay)
-# free_wheel()
-
-
